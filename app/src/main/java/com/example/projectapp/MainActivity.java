ce88fa97a1c5ab4522a824699cdb119548e26b28
a20fanma <a20fanma@student.his.se>
2024-05-20 10:44:21 +0200

    Made some changes in MainActivity

--- app/src/main/java/com/example/projectapp/MainActivity.java
+++ app/src/main/java/com/example/projectapp/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.recyclerview.widget.DividerItemDecoration;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -20,18 +21,17 @@ import java.util.List;
 
 public class MainActivity extends AppCompatActivity implements JsonTask.JsonTaskListener {
 
-    private final String JSON_FILE = "lake.json";
     RecyclerView lakeRecyclerView;
     private LakeAdapter adapter;
     private ArrayList<Lake> lakeArrayList;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        new JsonFile(this, this).execute(JSON_FILE);
+        String JSON_URL = "https://mobprog.webug.se/json-api?login=a20fanma";
+        new JsonTask(this).execute(JSON_URL);
 
         lakeRecyclerView = findViewById(R.id.recycler_view);
         lakeRecyclerView.setLayoutManager(new LinearLayoutManager(this));
@@ -39,8 +39,8 @@ public class MainActivity extends AppCompatActivity implements JsonTask.JsonTask
         lakeRecyclerView.setAdapter(adapter);
         lakeRecyclerView.addItemDecoration(new DividerItemDecoration(this, LinearLayoutManager.VERTICAL));
 
-        final Button button1 = findViewById(R.id.button1);
-        button1.setOnClickListener(new View.OnClickListener() {
+        final Button aboutButton = findViewById(R.id.aboutButton);
+        aboutButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(MainActivity.this, SecondActivity.class);
@@ -52,11 +52,13 @@ public class MainActivity extends AppCompatActivity implements JsonTask.JsonTask
 
     }
 
+    @SuppressLint("NotifyDataSetChanged")
     @Override
     public void onPostExecute(String json) {
         Gson gson = new Gson();
         Type type = new TypeToken<List<Lake>>() {}.getType();
         List<Lake> listOfLakes = gson.fromJson(json, type);
+        adapter.notifyDataSetChanged();
 
     }
 }
